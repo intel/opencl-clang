4274bad05bde95266c9059eb5c51d8a89937cb71
Yifei1 He <yifei1.he@intel.com>
2020-11-06 11:28:13 +0800

    Disable pch when using OCL 3.0

--- options_compile.cpp
+++ options_compile.cpp
@@ -282,7 +282,7 @@ std::string EffectiveOptionsFilter::processOptions(const OpenCLArgList &args,
       extMap.begin(), extMap.end(),
       [](decltype(*begin(extMap)) p) { return p.second == false; });
 
-  if (useModules) {
+  if (useModules && (iCLStdSet < 300)) {
     effectiveArgs.push_back("-fmodules");
     if (fp64Enable == 0) {
       if (szTriple.find("spir64") != szTriple.npos) {
